e31d6e651e9fb29a0b2de5eca0d8ecea3b3a256f
GalloSimone <gallosimonecz@gmail.com>
2025-02-24 15:05:51 +0100

    snack4 completato

--- snack4.js
+++ snack4.js
@@ -1,3 +1,15 @@
 // 🏆 Snack 4
 // Crea un generatore di funzioni creaTimer
 // Scrivi una funzione creaTimer che accetta un tempo (in ms) e restituisce una nuova funzione che avvia un setTimeout per stampare "Tempo scaduto!".
+
+function creaTimer(time){
+    function timeOut(){
+        setTimeout(()=>{
+            console.log("tempo finito");
+            
+        }, time);
+    }
+    return timeOut;
+}
+const timer=creaTimer(1000)
+timer();
