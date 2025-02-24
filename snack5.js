32064cb57f85d52a0ce9c20f9e74a6092486d1cd
GalloSimone <gallosimonecz@gmail.com>
2025-02-24 15:20:20 +0100

    snack 5 completato

--- snack5.js
+++ snack5.js
@@ -1,3 +1,16 @@
 // 🏆 Snack 5
 // Crea una funzione stampaOgniSecondo con setInterval.
 // Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+function stampaOgniSecondo(){
+    const interval= setInterval(()=>{
+        console.log("ciao");
+        
+    },1000);
+
+    setTimeout(()=>{
+        clearInterval(interval)
+    },10000)
+    
+}
+stampaOgniSecondo();
