77b8cb337d08ad9a080d6459abf860f62d669be8
GalloSimone <gallosimonecz@gmail.com>
2025-02-24 14:48:24 +0100

    snack3 completato

--- snack3.js
+++ snack3.js
@@ -1,3 +1,15 @@
 // 🏆 Snack 3
 // Crea una funzione eseguiOperazione
 // Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+function eseguiOperazione(a,b,operazione) {return operazione(a,b)}
+
+const somma =(a,b)=>a+b
+const sottrazione =(a,b)=>a-b
+const moltiplicazione =(a,b)=>a*b
+
+console.log(eseguiOperazione(4,2,moltiplicazione));
+
+
+
+
