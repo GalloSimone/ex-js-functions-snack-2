2e12254eb051c0836351aafd98e1cef7c63f836e
GalloSimone <gallosimonecz@gmail.com>
2025-02-24 14:20:21 +0100

    snack 2 completato

--- snack2.js
+++ snack2.js
@@ -1,3 +1,6 @@
 // 🏆 Snack 2
 // Crea una arrow function che calcola il quadrato di un numero.
 // Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga
+
+const quadrato = (a)=> a*a;
+console.log(quadrato(3));
